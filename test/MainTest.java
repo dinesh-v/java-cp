3535eb08926440fd5a29564f42b1c4faa1c7c79d
Dinesh V <dineshshabde@gmail.com>
2017-04-28 16:54:51 +0530

    Remove .idea folder so that the program can be used in Eclipse as well Compare user output with expectedOutput text file

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,17 +1,54 @@
-import org.junit.Assert;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    @Test
+    public void out() {
+        System.out.print("hello");
+        assertEquals("hello", outContent.toString());
+    }
+
+    @Test
+    public void err() {
+        System.err.print("hello again");
+        assertEquals("hello again", errContent.toString());
+    }
+
     @Test
     public void main() throws Exception {
+        // Set input
         File inputFile = new File(MainTest.class.getResource("input.txt").toURI());
         final FileInputStream fileInputStream = new FileInputStream(inputFile);
         System.setIn(fileInputStream);
+        // Get expected output
+        String expectedOutput = new String(Files.readAllBytes(Paths.get(MainTest.class.getResource("expectedOutput.txt").toURI())));
         Main.main(null);
-        Assert.assertTrue(true);
-
+        assertEquals(expectedOutput, outContent.toString().trim());
     }
 }
